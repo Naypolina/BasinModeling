614b871d41aaaf361a66ea112091e8c7411f0703
Naypolina <naypolina@yandex.ru>
2020-11-20 21:21:02 +0300

    Исправлена отрисовка осей

--- T(x)/T(x).py
+++ T(x)/T(x).py
@@ -4,7 +4,7 @@ import matplotlib.ticker as ticker
 
 T_0 = 20
 L = 10
-Theta_0 = 0.5
+Theta_0 = 5
 
 x = np.linspace(0, L, 200)
 y = T_0 + Theta_0 * np.sin(np.pi/L * x)
@@ -14,11 +14,11 @@ fig, ax = plt.subplots()
 ax.plot(x, y, linewidth = 2)
 ax.grid()
 
-ax.xaxis.set_major_locator(ticker.MultipleLocator(2))
-ax.xaxis.set_minor_locator(ticker.MultipleLocator(1))
+ax.xaxis.set_major_locator(ticker.MultipleLocator(L/5))
+ax.xaxis.set_minor_locator(ticker.MultipleLocator(L/10))
 
-ax.yaxis.set_major_locator(ticker.MultipleLocator(0.1))
-ax.yaxis.set_minor_locator(ticker.MultipleLocator(0.05))
+ax.yaxis.set_major_locator(ticker.MultipleLocator(Theta_0/5))
+ax.yaxis.set_minor_locator(ticker.MultipleLocator(Theta_0/10))
 
 ax.grid(which = 'major',
         color = 'k')
