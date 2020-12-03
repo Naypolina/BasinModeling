bb08f0a71f545154d152b038cba9b1f6a4dfaf1b
Naypolina <naypolina@yandex.ru>
2020-12-03 18:45:27 +0300

    Добавлено сравнение численного и аналитического решения

--- conductivity_equation/conductivity_equation.py
+++ conductivity_equation/conductivity_equation.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.integrate as integrate
 from mpl_toolkits.mplot3d import Axes3D
 
 # параметры образца
@@ -10,7 +11,7 @@ N = 3000
 M = 50
 
 # шаги переменных
-t = 10
+tau = 10
 h = L/M
 
 # константы для мрамора (брала средние значения в диапазоне)
@@ -19,6 +20,7 @@ rho = 2.3
 Lambda = 2.8
 a = Lambda/(c*rho)
 
+# численное решение уравнения теплопролводности
 T = np.array([[0] * M for i in range(N)]).astype('float64')
 
 for m in range(0, M):
@@ -30,7 +32,7 @@ for n in range(1, N):
 
 for i, ti in enumerate(T):
     if i>0:
-        ti[1:-1] = T[i-1][1:-1]+a*t/(h**2)*(T[i-1][:-2]-2*T[i-1][1:-1]+T[i-1][2:])
+        ti[1:-1] = T[i-1][1:-1] + a*tau/(h**2) * (T[i-1][:-2] - 2 * T[i-1][1:-1] + T[i-1][2:])
         # print(T[n][m])
 
 x = []
@@ -43,20 +45,36 @@ for n in range(0, N):
     z.append([])
     for m in range(0, M):
         x[-1].append(m * h)
-        y[-1].append(n * t)
+        y[-1].append(n * tau)
         z[-1].append(T[n][m])
 
 x = np.array(x)
 y = np.array(y)
 z = np.array(z)
 
-
-fig = plt.figure()
+plt.figure(1)
 ax = plt.axes(projection="3d")
 ax.plot_surface(x, y, z)
 ax.set_xlabel('Координата в образце (см)')
 ax.set_ylabel('Время (с)')
-ax.set_zlabel('Температура (ºС)')
+ax.set_zlabel('Температура числ (ºС)')
 plt.show()
 
 
+# аналитическое решение уравнения теплопролводности
+def T_an(x, t):
+    return T_0 + Theta_0 * np.exp(- (np.pi / L) ** 2 * a * t) * np.sin(np.pi * x / L)
+
+plt.figure(2)
+ax = plt.axes(projection="3d")
+
+x = np.linspace(0, L, M)
+y = np.linspace(1, tau * N, N)
+X, Y = np.meshgrid(x, y)
+Z = T_an(X, Y)
+
+ax.plot_surface(X, Y, Z, color='green')
+ax.set_xlabel('Координата в образце (см)')
+ax.set_ylabel('Время (с)')
+ax.set_zlabel('Температура аналит (ºС)')
+plt.show()
