dd728c28c02f5ea9fe79c9dfce5c783372743910
Naypolina <naypolina@yandex.ru>
2020-12-03 04:54:57 +0300

    Оптимизированы вычисления

--- conductivity_equation/conductivity_equation.py
+++ conductivity_equation/conductivity_equation.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 # параметры образца
 L = 10
 T_0 = 20
 Theta_0 = 5
-N = 10000
+N = 3000
 M = 50
 
 # шаги переменных
@@ -18,7 +19,8 @@ rho = 2.3
 Lambda = 2.8
 a = Lambda/(c*rho)
 
-T = [[0] * M for i in range(N)]
+T = np.array([[0] * M for i in range(N)]).astype('float64')
+
 for m in range(0, M):
     T[0][m] = T_0 + Theta_0 * np.sin(np.pi / L * m * h)
 
@@ -26,9 +28,9 @@ for n in range(1, N):
     T[n][0] = T_0
     T[n][M - 1] = T_0
 
-for n in range(1, N):
-    for m in range(1, M - 1):
-        T[n][m] = T[n - 1][m] + a*t/(h**2) * (T[n - 1][m - 1] - 2 * T[n - 1][m] + T[n - 1][m + 1])
+for i, ti in enumerate(T):
+    if i>0:
+        ti[1:-1] = T[i-1][1:-1]+a*t/(h**2)*(T[i-1][:-2]-2*T[i-1][1:-1]+T[i-1][2:])
         # print(T[n][m])
 
 x = []
