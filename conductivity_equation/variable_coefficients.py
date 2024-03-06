551414a98084705734d63d23d91023c51e936afa
Polina Naydenova <naypolina@yandex.ru>
2024-03-06 12:28:44 +0300

    Исправлена схема решения

--- conductivity_equation/variable_coefficients.py
+++ conductivity_equation/variable_coefficients.py
@@ -2,17 +2,20 @@ import numpy as np
 import conditions as cond
 
 
+# выбор шага по времени
+tau = cond.tau([cond.a(cond.T_0)])
+
 # численное решение уравнения теплопролводности
 T = np.zeros([cond.N, cond.M + 1])
-a_x = np.zeros(cond.M + 1)
+a_T = np.zeros(cond.M + 1)
 
 m = np.arange(cond.M + 1)
 T[0][m] = cond.T_0 + cond.Theta_0 * np.sin(np.pi / cond.L * m * cond.h)
 T[:, 0] = cond.T_0
 T[:, -1] = cond.T_0
-a_x[m] = cond.a(m * cond.h + cond.h / 2)
 
 for i, ti in enumerate(T):
     if i > 0:
-        ti[1:-1] = T[i-1][1:-1] + cond.tau / (cond.h ** 2) \
-                   * (a_x[:-2] * T[i-1][:-2] - (a_x[:-2] + a_x[2:]) * T[i-1][1:-1] + a_x[2:] * T[i-1][2:])
+        a_T = cond.a((T[i-1][:-1] + T[i-1][1:]) / 2)
+        ti[1:-1] = T[i-1][1:-1] + tau / (cond.h ** 2) \
+                   * (a_T[:-1] * T[i-1][:-2] - (a_T[:-1] + a_T[1:]) * T[i-1][1:-1] + a_T[1:] * T[i-1][2:])
