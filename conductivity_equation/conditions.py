e8fc2e2bd1fc1fe1873f1bac3611c998c9198bc9
Polina Naydenova <naypolina@yandex.ru>
2024-03-06 12:25:28 +0300

    добавлены новые условия

--- conductivity_equation/conditions.py
+++ conductivity_equation/conditions.py
@@ -1,6 +1,3 @@
-import numpy as np
-
-
 # параметры образца
 L = 10
 T_0 = 20
@@ -12,9 +9,9 @@ N = 10000
 M = 50
 
 # константы для известняка
-c1 = 920
-rho1 = 2.1
-Lambda1 = 2.56
+c1 = 920  # Дж / (кг * К)
+rho1 = 2.1  # г / см^3
+Lambda1 = 2.56  # Вт / (м * К)
 a1 = Lambda1 / (c1 * rho1)
 
 # константы для глины
@@ -41,6 +38,12 @@ rho_w = 1.0
 Lambda_w = 0.56
 a_w = Lambda_w / (c_w * rho_w)
 
+# константы для н-пентана
+c_p = 2280
+rho_p = 0.626
+Lambda_p = 0.114
+a_p = Lambda_p / (c_p * rho_p)
+
 # пористость
 phi = 0.4
 
@@ -52,14 +55,21 @@ a_phi_air = pow(Lambda1, 1 - phi) * pow(Lambda_air, phi) / (c1 * (1 - phi) + c_a
 a_phi_w = pow(Lambda1, 1 - phi) * pow(Lambda_w, phi) / (c1 * (1 - phi) + c_w * phi) \
           / (rho1 * (1 - phi) + rho_w * phi)
 
-
-# функциональная зависимость теплопроводности от глубины
-def a(x):
-    return Lambda1 * np.exp(-0.8 * x / L) / (c1 * rho1)
+# функциональная зависимость теплопроводности от температуры (известняк)
+def a(temp):
+    Lambda = 9.03 * pow(temp, -0.25)
+    return Lambda / (c1 * rho1)
 
 
 # шаги переменных с учетом проверки устойчивости схемы
-tau = 3
+tau_init = 3
 h = L / M
-if tau > pow(h, 2) / (2 * max(a1, a2, a3, a_phi_w, a_phi_air, a(L + h / 2))):
-    tau = pow(h, 2) // (2 * max(a1, a2, a3, a_phi_w, a_phi_air, a(L + h / 2)))
+
+
+# функция проверки шага по времени
+# на вход подается список из коэффициентов a, используемых при решении
+def tau(a_list):
+    if tau_init > pow(h, 2) / (2 * max(a_list)):
+        return pow(h, 2) // (2 * max(a_list))
+    else:
+        return tau_init
