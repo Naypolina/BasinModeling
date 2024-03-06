5cb18067c32b8badae7f090c77f33c6b35f921cb
Polina Naydenova <naypolina@yandex.ru>
2024-03-06 12:29:29 +0300

    Добавлены аргументы пределов по осям

--- conductivity_equation/functions.py
+++ conductivity_equation/functions.py
@@ -1,12 +1,24 @@
 import matplotlib.pyplot as plt
+from matplotlib.ticker import AutoMinorLocator
 
-def graph_setup(x_label, y_label, g_title, x_max):
-    plt.xlabel(x_label, fontsize=16)
-    plt.ylabel(y_label, fontsize=16)
-    plt.title(g_title, fontsize=16)
-    plt.legend(fontsize=14, loc='upper right')
+
+def plot_setup(x_label, y_label, g_title, xlims, ylims, axis, bbox):
+    plt.xlabel(x_label, fontsize=26)
+    plt.ylabel(y_label, fontsize=26)
+    plt.title(g_title, fontsize=24, fontweight='bold')
+    plt.legend(fontsize=18, bbox_to_anchor=bbox, loc='upper right',
+               shadow=True, fancybox=True)
+    plt.tight_layout()
+    plt.tick_params(axis='both', which='major', labelsize=20)
+    plt.xlim(xlims)
+    plt.ylim(ylims)
     plt.minorticks_on()
-    plt.xlim([0., x_max])
-    plt.ylim([20., 26.])
-    plt.grid(which='major', color='#C0C0C0')
-    plt.grid(which='minor', ls='--', color='#D3D3D3')
+    axis.xaxis.set_minor_locator(AutoMinorLocator(2))
+    axis.yaxis.set_minor_locator(AutoMinorLocator(2))
+    plt.grid(which='major', color='#C0C0C0', linewidth=2)
+    plt.grid(which='minor', ls='--', color='#D3D3D3', linewidth=2)
+
+def text_on_plot(axis, x_loc, y_loc, text):
+    axis.text(x_loc, y_loc, text, fontsize=26, style='italic',
+             bbox={'facecolor': '#C0C0C0', 'alpha': 0.35, 'pad': 5})
+
